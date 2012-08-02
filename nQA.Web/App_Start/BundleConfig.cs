12490ca7492cdcd42fa7144337b1b7f99725fb5a
adamlepkowski <adam.lepkowski@gmail.com>
2012-08-02 15:01:37 +0200

    Added initial web project

---
+++ nQA.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace nQA.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.EnableDefaultBundles();
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/themes/base/css").Include(
+                        "~/Content/themes/base/*.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
