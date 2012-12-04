6cc366ce405483408974a0a29026b26489885831
adamlepkowski <adam.lepkowski@gmail.com>
2012-12-04 20:14:03 +0100

    Added first test for Account Controller : Login_RequestFromTheUser_OpenLoginPage

---
+++ Tests/nQA.Web.Tests/AccountControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+//using NSubstitute;
+using NSubstitute;
+using NUnit.Framework;
+using nQA.Web.Controllers;
+using nQA.Web.Services;
+
+namespace nQA.Web.Tests
+{
+    [TestFixture]
+    public class AccountControllerTest
+    {
+        [Test]
+        public void Login_RequestFromTheUser_OpenLoginPage()
+        {
+            var openIdMembershipService = Substitute.For<IOpenIdMembershipService>();
+            openIdMembershipService.GetResponse().Returns(info => null);
+
+            var controller = new AccountController(openIdMembershipService);
+
+            var actionResult = controller.Login() as ViewResult;
+
+            Assert.IsEmpty(actionResult.ViewName);
+        }
+    }
+}
