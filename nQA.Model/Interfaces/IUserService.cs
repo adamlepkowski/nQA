202e0a80f008f072e1a9b6283feb90152df0e383
adamlepkowski <adam.lepkowski@gmail.com>
2012-12-20 12:56:51 +0100

    Added user service interface without implementation

---
+++ nQA.Model/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using nQA.Model.Entities;
+
+namespace nQA.Model.Interfaces
+{
+    public interface IUserService
+    {
+        User Provide(string claimedIdentifier, string nickname, string fullName, string email);
+    }
+}
---
+++ nQA.Model/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using nQA.Model.Entities;
+using nQA.Model.Interfaces;
+
+namespace nQA.Model.Services
+{
+    public class UserService : IUserService
+    {
+        public User CreateUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User Provide(string claimedIdentifier, string nickname, string fullName, string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User CreateUser(string claimedIdentifier, string nickname, string fullName, string email)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
