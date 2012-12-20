b8bc07af48fbaeb9b450762e20772c6e5fd39a88
adamlepkowski <adam.lepkowski@gmail.com>
2012-12-20 12:56:00 +0100

    Added User entity

---
+++ nQA.Model/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace nQA.Model.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string Login { get; set; }
+
+        public string FullName { get; set; }
+
+        public bool Disabled { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string City { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime LastVisitDate { get; set; }
+
+        public bool ShowDetails { get; set; }
+
+        public string Website { get; set; }
+
+        public string ShortDescription { get; set; }
+    }
+}
