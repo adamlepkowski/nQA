ff3f50534b0ad6de336ef3acf72049b581580581
adamlepkowski <adam.lepkowski@gmail.com>
2012-12-20 12:54:49 +0100

    Added generic repository interfaces without implementation

---
+++ nQA.Model/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace nQA.Model.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> FindAll();
+        IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate);
+        T FindById(int id);
+        void Add(T newEntity);
+        void Remove(T entity);
+    }
+}
---
+++ nQA.Model/Repositories/GenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+using nQA.Model.Interfaces;
+
+namespace nQA.Model.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class, new()
+    {
+        public IEnumerable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T newEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
