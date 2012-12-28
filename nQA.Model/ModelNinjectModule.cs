be4892699c0b1e43ac482f43cbc8137e55129ab4
adamlepkowski <adam.lepkowski@gmail.com>
2012-12-28 22:18:46 +0100

    Added IOC configuration

---
+++ nQA.Model/ModelNinjectModule.cs
@@ -0,0 +1,17 @@
+﻿using Ninject.Modules;
+using nQA.Model.Interfaces;
+using nQA.Model.Repositories;
+using nQA.Model.Services;
+
+namespace nQA.Model
+{
+    public class ModelNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<DatabaseContext>().ToSelf();//TODO: Instance per request
+            Bind(typeof(IRepository<>)).To(typeof(GenericRepository<>));
+            Bind<IUserService>().To<UserService>();
+        }
+    }
+}
