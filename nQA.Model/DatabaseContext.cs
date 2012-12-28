03fb4b4cbfba1f539fae67b9ad269a99e8e3b357
adamlepkowski <adam.lepkowski@gmail.com>
2012-12-28 21:31:17 +0100

    Added database context

---
+++ nQA.Model/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using nQA.Model.Entities;
+
+namespace nQA.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("nQADatabase")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
